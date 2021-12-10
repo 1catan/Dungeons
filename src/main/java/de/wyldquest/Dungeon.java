b85c491adb902ef14276f889bc7f99f3d0100add
Catan <92204294+1catan@users.noreply.github.com>
2021-12-10 18:22:46 +0100

    Initial commit

---
+++ src/main/java/de/wyldquest/Dungeon.java
@@ -0,0 +1,16 @@
+package de.wyldquest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Dungeon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
